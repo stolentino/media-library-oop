85da4514934306a863430678270564e907fd0957
Samuel <stotino2000@yahoo.com>
2021-01-04 17:37:17 -0800

    use GET to add pageTitle for catalog page

--- catalog.php
+++ catalog.php
@@ -1,5 +1,16 @@
 <?php 
 $pageTitle = "Full Catalog";
+
+if (isset($_GET["cat"])){
+    if($_GET["cat"] == "books") {
+        $pageTitle = "Books";
+    } else if ($_GET["cat"] == "movies") {
+        $pageTitle = "Movies";
+    } else if ($_GET["cat"] == "music") {
+        $pageTitle = "Music";
+    }
+}
+
 include("inc/header.php"); 
 ?>
 
