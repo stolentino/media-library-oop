955837d10b985e12b76b0124d298453f4ce94ba7
Samuel <stotino2000@yahoo.com>
2021-01-04 16:18:54 -0800

    creater header file and use include

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
-
-<html>
-<head>
-	<title><?php echo "Personal Media Library" ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-<body>
-
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
-
-			<ul class="nav">
-                <li class="books"><a href="catalog.php?cat=books">Books</a></li>
-                <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
-                <li class="music"><a href="catalog.php?cat=music">Music</a></li>
-                <li class="suggest"><a href="suggest.php">Suggest</a></li>
-            </ul>
-
-		</div>
-
-	</div>
-
-	<div id="content">
+<?php include("inc/header.php"); ?>
 		<div class="section catalog random">
 
 			<div class="wrapper">
@@ -38,7 +13,7 @@
 
 		</div>
 
-	</div>
+	</div><!-- end content -->
 
 	<div class="footer">
 
